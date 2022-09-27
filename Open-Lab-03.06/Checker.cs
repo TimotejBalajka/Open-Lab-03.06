a3b59294e1e7ba487f0aa52d97f62104471be710
TimotejBalajka <113421940+TimotejBalajka@users.noreply.github.com>
2022-09-27 09:23:31 +0200

    Update Checker.cs

--- Open-Lab-03.06/Checker.cs
+++ Open-Lab-03.06/Checker.cs
@@ -7,9 +7,13 @@ namespace Open_Lab_03._06
         public bool HasSpaces(string str)
         {
             if (str.Contains(" "))
+            {
                 return true;
+            }
             else
+            {
                 return false;
+            }
         }
     }
 }
